76241b8feae2b42907fe63202cac677df389048d
Luka Simić <git@kocka.tech>
2023-12-29 23:24:03 +0100

    Context changes for list and grid editing.

--- src/util/Context.ts
+++ src/util/Context.ts
@@ -1,62 +1,139 @@
 import {createContext} from 'react';
-import {IniFile, IniValue} from './ini';
+import {GMValueType, IniFile, IniGrid, IniList, IniValue} from './ini';
 import { SaveFileName, SaveFiles, emptySaves } from './save';
 
 type SaveContextAction = {
-    type: 'change',
+    type: 'change';
     save: SaveFileName;
     section: string;
     option: string;
     value: IniValue;
+    index?: number | [number, number];
 } | {
     type: 'add';
     save: SaveFileName;
     section: string;
     option: string;
-    value: IniValue;
+    value: string | number | boolean;
 } | {
     type: 'load';
     save: SaveFileName;
     value: IniFile;
+} | {
+    type: 'remove';
+    save: SaveFileName;
+    section: string;
+    option: string;
+    index: number;
+} | {
+    type: 'draw';
+    save: SaveFileName;
+    section: string;
+    option: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    color: number;
 };
 
 export function contextReducer(state: SaveFiles, action: SaveContextAction): SaveFiles {
-    // For a lack of a better deep-copy method.
-    const newState: SaveFiles = JSON.parse(JSON.stringify(state));
     if (action.type === 'load') {
-        newState[action.save].loaded = true;
-        newState[action.save].data = action.value;
-        return newState;
+        return {
+            ...state,
+            [action.save]: {
+                data: action.value,
+                loaded: true
+            }
+        };
     }
     const {save, section, option} = action;
     if (!state[save].loaded) {
         // This should not happen.
         return state;
     }
-    if (!newState[save].data.order.includes(section)) {
-        newState[save].data.order.push(section);
-    }
-    const sectionData = newState[save].data.data[section] || {
+    const sectionData = state[save].data.data[section] || {
         data: {},
         order: []
     };
+    if (!state[save].data.data[section]) {
+        state[save].data.order.push(section);
+    }
+    const optionData = sectionData.data[option];
+    if (!optionData) {
+        sectionData.order.push(option);
+    }
+    // For a lack of a better deep-copy method.
+    let newOptionData: IniValue = JSON.parse(JSON.stringify(optionData));
     switch (action.type) {
         case 'change':
-            if (!sectionData.order.includes(option)) {
-                sectionData.order.push(option);
+            if (typeof action.index === 'number') {
+                (newOptionData as IniList)[action.index]
+                    .value = (action.value as string | number);
+            } else {
+                newOptionData = action.value;
             }
-            sectionData.data[option] = action.value;
-            return newState;
+            break;
         case 'add':
-            if (!sectionData.order.includes(option) || !Array.isArray(sectionData.data[option])) {
-                sectionData.order.push(option);
-                sectionData.data[option] = [];
+            if (!Array.isArray(newOptionData)) {
+                return state;
             }
-            (sectionData.data[option] as IniValue[]).push(action.value);
+            (newOptionData as IniList).push(
+                typeof action.value === 'string' ?
+                    {
+                        type: GMValueType.STRING,
+                        value: action.value
+                    } :
+                    typeof action.value === 'boolean' ?
+                        {
+                            type: GMValueType.BOOL,
+                            value: Number(action.value)
+                        } :
+                        {
+                            type: GMValueType.REAL,
+                            value: action.value
+                        }
+            );
+            break;
+        case 'remove':
+            if (!Array.isArray(newOptionData)) {
+                return state;
+            }
+            (newOptionData as IniList).splice(action.index, 1);
+            break;
+        case 'draw':
+            const grid = newOptionData as IniGrid;
+            const gridWidth = grid[0].length;
+            const gridHeight = grid.length;
+            const {x, y, width, height, color} = action;
+            for (let dx = x; dx > 0 && dx < gridWidth && dx < x + width; ++dx) {
+                for (let dy = y; dy > 0 && dy < gridHeight && dy < y + height; ++dy) {
+                    grid[dy][dx].value = color;
+                }
+            }
             return state;
         default:
             return state;
     }
+    return {
+        ...state,
+        [save]: {
+            ...state[save],
+            data: {
+                ...state[save].data,
+                data: {
+                    ...state[save].data.data,
+                    [section]: {
+                        ...sectionData,
+                        data: {
+                            ...sectionData.data,
+                            [option]: newOptionData
+                        }
+                    }
+                }
+            }
+        }
+    };
 }
 
 interface SaveContextWithDispatch {
