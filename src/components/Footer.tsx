10c3a0ab46572fd558d541b903554884ee005029
Luka Simić <git@kocka.tech>
2024-01-04 23:27:44 +0100

    Bump version and display it in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import pkg from '../../package.json';
 
 const Footer: React.FC = () => {
     return <footer className="mt-10 border-t-2 border-dashed pt-2">
         Made by <a href="https://kocka.tech/">KockaAdmiralac</a>. Source code
         available on <a href="https://github.com/KockaAdmiralac/uty-save-editor">
         GitHub</a>. Please submit feedback through <a href="https://github.com/KockaAdmiralac/uty-save-editor/issues">
-        GitHub Issues</a>.
+        GitHub Issues</a>. Current version: <em>{pkg.version}</em>.
     </footer>
 };
 
